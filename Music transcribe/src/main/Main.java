ec616c36fe6a1a52b3a1e89a71fe1da3dbfa3249
Daniel <nxu231@aucklanduni.ac.nz>
2020-11-15 10:46:00 +1300

    made a pretty(ish) login page

---
+++ Music transcribe/src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+import javafx.*;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void start(Stage arg0) throws Exception{
+		
+	}
+
+}
