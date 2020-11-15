bbf9e1068d535e02ddd87d35d259e7f178699a97
Daniel <nxu231@aucklanduni.ac.nz>
2020-11-15 17:41:01 +1300

    can now make people browse for random stuff, bit of a joke now

--- Music transcribe/src/main/Menu_Controller.java
+++ Music transcribe/src/main/Menu_Controller.java
@@ -1,14 +1,56 @@
 package main;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
-public class Menu_Controller {
+import java.awt.Desktop;
+import java.awt.desktop.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class Menu_Controller implements Initializable {
 	private Button closeButton;
 	
 	public void closeButtonOnAction(ActionEvent event) {
-		Stage stage = (Stage) closeButton.getScene().getWindow();
-		stage.close();
+		System.out.println("Hello?");
+		Desktop d = Desktop.getDesktop();
+		try {
+			d.browse(new URL("https://www.youtube.com/watch?v=wpV-gGA4PSk").toURI());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
 	}
 }
