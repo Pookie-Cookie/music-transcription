eba67afa7b6f91bf3bc65d207ee80145b1ddd191
Daniel <nxu231@aucklanduni.ac.nz>
2020-11-15 17:09:57 +1300

    made some more main menu functionality changes(launches into a window now!)

--- Music transcribe/src/main/Main.java
+++ Music transcribe/src/main/Main.java
@@ -2,17 +2,26 @@ package main;
 import javafx.*;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 
 public class Main extends Application{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	@Override
+	public void start(Stage stage) throws Exception{
+		Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
 	}
 	
-	public void start(Stage arg0) throws Exception{
-		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
 	}
 
 }
--- Music transcribe/src/main/Menu_Controller.java
+++ Music transcribe/src/main/Menu_Controller.java
@@ -1,5 +1,14 @@
 package main;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class Menu_Controller {
-
+	private Button closeButton;
+	
+	public void closeButtonOnAction(ActionEvent event) {
+		Stage stage = (Stage) closeButton.getScene().getWindow();
+		stage.close();
+	}
 }
