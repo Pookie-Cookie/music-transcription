33197a2d051cc4ed098449ad3a97b70fe03921dc
Daniel <nxu231@aucklanduni.ac.nz>
2020-11-27 00:29:30 +1300

    added checks for already existing username and email

--- Music transcribe/src/main/Sign_Up_Controller.java
+++ Music transcribe/src/main/Sign_Up_Controller.java
@@ -1,5 +1,7 @@
 package main;
 
+import javax.swing.JOptionPane;
+
 import org.bson.Document;
 
 import com.mongodb.DBCollection;
@@ -17,15 +19,23 @@ public class Sign_Up_Controller {
 	DBCollection coll = Main.mongocoll;
 	
 	@FXML
-	private TextField username;
+	private TextField usernameBox;
+	@FXML 
+	private TextField emailBox;
 	
 	public void sign_up_button(ActionEvent event) {
 		DBCursor fi = coll.find();
 		while(fi.hasNext()) {
 			DBObject obj = fi.next();
 			String Username = (String) obj.get("username");
-			if (username.getText() == Username) {
+			String email = (String) obj.get("email");
+			if (usernameBox.getText().equals(Username)) {
 				System.out.println("found");
+				JOptionPane.showMessageDialog(null, "Username already exists" );
+			}
+			else if (emailBox.getText().equals(email)) {
+				System.out.println("found1");
+				JOptionPane.showMessageDialog(null, "Email already exists in system");
 			}
 		}
 	}
