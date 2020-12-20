632a7d8990586b184412f2af645ac3f6bbfa4141
Daniel <nxu231@aucklanduni.ac.nz>
2020-12-20 23:17:28 +1300

    added checks for empty sign up boxes in sign up

--- Music transcribe/src/main/Sign_Up_Controller.java
+++ Music transcribe/src/main/Sign_Up_Controller.java
@@ -31,45 +31,52 @@ public class Sign_Up_Controller {
 	@FXML
 	private TextField passConfirmBox;
 	
+	
+	
+	
 	public void sign_up_button(ActionEvent event) throws IOException, NoSuchAlgorithmException {
-		DBCursor fi = coll.find();
-		String pass_hash = Helpers.SHA_Hash.toHexString(Helpers.SHA_Hash.getSHA(passwordBox.getText()));
-		while(fi.hasNext()) {
-			DBObject obj = fi.next();
-			String Username = (String) obj.get("username");
-			String email = (String) obj.get("email");
-			if (usernameBox.getText().equals(Username)) {
-				System.out.println("found");
-				JOptionPane.showMessageDialog(null, "Username already exists" );
+		if(!usernameBox.getText().isBlank() && !emailBox.getText().isBlank() && !passwordBox.getText().isBlank() && !passConfirmBox.getText().isBlank()){
+			DBCursor fi = coll.find();
+			String pass_hash = Helpers.SHA_Hash.toHexString(Helpers.SHA_Hash.getSHA(passwordBox.getText()));
+			while(fi.hasNext()) {
+				DBObject obj = fi.next();
+				String Username = (String) obj.get("username");
+				String email = (String) obj.get("email");
+				if (usernameBox.getText().equals(Username)) {
+					System.out.println("found");
+					JOptionPane.showMessageDialog(null, "Username already exists" );
+				}
+				else if (emailBox.getText().equals(email)) {
+					System.out.println("found1");
+					JOptionPane.showMessageDialog(null, "Email already exists in system");
+				}
 			}
-			else if (emailBox.getText().equals(email)) {
-				System.out.println("found1");
-				JOptionPane.showMessageDialog(null, "Email already exists in system");
+			if (!emailBox.getText().contains("@") || !emailBox.getText().contains("@")) {
+				JOptionPane.showMessageDialog(null, "Please enter a valid email address");
 			}
-		}
-		if (!emailBox.getText().contains("@") || !emailBox.getText().contains("@")) {
-			JOptionPane.showMessageDialog(null, "Please enter a valid email address");
-		}
-		else if (usernameBox.getText() == "" || passwordBox.getText() == "" || emailBox.getText() == "" || passConfirmBox.getText() == "" ) {
-			JOptionPane.showMessageDialog(null, "Please fill out all fields");
-		}
-		else if (!passwordBox.getText().equals(passConfirmBox.getText())){
-			JOptionPane.showMessageDialog(null, "The passwords do not match!");
+			else if (usernameBox.getText() == "" || passwordBox.getText() == "" || emailBox.getText() == "" || passConfirmBox.getText() == "" ) {
+				JOptionPane.showMessageDialog(null, "Please fill out all fields");
+			}
+			else if (!passwordBox.getText().equals(passConfirmBox.getText())){
+				JOptionPane.showMessageDialog(null, "The passwords do not match!");
+			}
+			else {
+				BasicDBObject doc = new BasicDBObject();
+				doc.append("username", usernameBox.getText());
+				doc.append("password", pass_hash);
+				doc.append("email", emailBox.getText());
+				coll.insert(doc);
+				
+				Parent TranscribeParent = FXMLLoader.load(getClass().getResource("Transcribe_tab.fxml"));
+				Scene TranscribeScene = new Scene(TranscribeParent);
+				
+				Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+				window.setScene(TranscribeScene);
+				window.show();
+			}
 		}
 		else {
-			BasicDBObject doc = new BasicDBObject();
-			doc.append("username", usernameBox.getText());
-			doc.append("password", pass_hash);
-			doc.append("email", emailBox.getText());
-			coll.insert(doc);
-			
-			Parent TranscribeParent = FXMLLoader.load(getClass().getResource("Transcribe_tab.fxml"));
-			Scene TranscribeScene = new Scene(TranscribeParent);
-			
-			Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-			window.setScene(TranscribeScene);
-			window.show();
+			JOptionPane.showMessageDialog(null, "One of the boxes are empty, please check your input again!");
 		}
-		
 	}
 }
